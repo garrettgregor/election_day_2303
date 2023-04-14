0ebb2ec7eca428f2989e066ba7a2368078d456f4
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-14 09:52:02 -0600

    verify that register candidate can register multiple candidates and collect them

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -16,12 +16,21 @@ RSpec.describe Race do
   describe "register_candidate!" do
     it "registers a candidate for the race" do
       candidate1 = @race.register_candidate!({name: "Diana D", party: :democrat})
-
+      # => #<Candidate:0x00007f9edf376c90...>
       expect(candidate1.class).to be Candidate
       expect(candidate1.class).to eq(Candidate)
-      # can't use be_a, be_an, or be_an_instance_of because it is not an instance 
+      # can't use be_a, be_an, or be_an_instance_of because it is not an instance of Candidate
       expect(candidate1.name).to eq("Diana D")
       expect(candidate1.party).to eq(:democrat)
+    end
+    
+    it "can register multiple candidates and track those candidates" do
+      candidate1 = @race.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = @race.register_candidate!({name: "Roberto R", party: :republican})
+      # => #<Candidate:0x00007f9edf386780...>
+
+      expect(@race.candidates).to eq([candidate1, candidate2])
+      # => [#<Candidate:0x00007f9edf376c90...>, #<Candidate:0x00007f9edf386780...>]
     end
   end
 end
