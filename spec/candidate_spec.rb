3025f5f1eb6e6faddb33e3663e951aacf361e8d1
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-14 09:18:34 -0600

    Fix mistake in initialize with party and add vote for method to candidate

--- spec/candidate_spec.rb
+++ spec/candidate_spec.rb
@@ -9,8 +9,24 @@ RSpec.describe Candidate do
     it "exists and has attributes" do
       expect(@diana).to be_a(Candidate)
       expect(@diana.name).to eq("Diana D")
-      expect(@diana.party).to eq("Diana D")
+      expect(@diana.party).to eq(:democrat)
       expect(@diana.votes).to eq(0)
+    end
+  end
+ 
+  describe "vote_for!" do
+    it "adds to votes count for candidate" do
+      expect(@diana.votes).to eq(0)
+
+      @diana.vote_for!
+      @diana.vote_for!
+      @diana.vote_for!
+      
+      expect(@diana.votes).to eq(3)
+      
+      @diana.vote_for!
+      
+      expect(@diana.votes).to eq(4)
     end
   end
 end
